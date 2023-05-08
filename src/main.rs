51b33aa600db271f618d3cd697993ae434948558
Nathan Vance <Natevance01@gmail.com>
2023-05-08 13:27:16 -0500

    created main menu and retrevial of input from the user

--- src/main.rs
+++ src/main.rs
@@ -58,4 +58,39 @@ fn get_user_input() -> Option<String> {
     }
 }
 
-fn main() {}
+enum MainMenu {
+    AddBill,
+    ViewBill,
+}
+
+impl MainMenu {
+    ///Converst user input into Main Menu varient
+    fn from_str(input: &str) -> Option<MainMenu> {
+        match input {
+            "1" => Some(MainMenu::AddBill),
+            "2" => Some(MainMenu::ViewBill),
+            _ => None,
+        }
+    }
+    fn show() {
+        println!("");
+        println!("-- Bill Manager --");
+        println!("1) Add Bill");
+        println!("2) View Bill");
+    }
+}
+
+fn main() {
+    loop {
+        //Display Menu
+        MainMenu::show();
+        let input = get_user_input().expect("no data entered");
+        match MainMenu::from_str(input.as_str()) {
+            Some(MainMenu::AddBill) => (),
+            Some(MainMenu::ViewBill) => (),
+            None => return,
+        }
+        //Make a choice based on user input
+        //create bill struct
+    }
+}
