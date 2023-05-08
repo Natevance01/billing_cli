4398aaec894db622f98f3614ac0d69ab6131e7a3
Nathan Vance <Natevance01@gmail.com>
2023-05-08 13:10:14 -0500

    created user input function

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,19 @@ impl Bill {
         Self { name, amount }
     }
 }
-fn main() {
-    loop {}
+/// Gets user input from stdin
+fn get_user_input() -> Option<String> {
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer).is_err() {
+        println!("Please enter your data again")
+    }
+    //trims input
+    let input = buffer.trim().to_owned();
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
 }
+
+fn main() {}
